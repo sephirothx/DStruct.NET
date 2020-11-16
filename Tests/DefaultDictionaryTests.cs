59930155ba298839015eb5106b1138c0e1028627
Stefano Prosperi <step.prosperi@gmail.com>
2020-11-16 17:01:58 +0100

    Added tests for DefaultDictionary

---
+++ Tests/DefaultDictionaryTests.cs
@@ -0,0 +1,22 @@
+﻿using DStruct;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class DefaultDictionaryTests
+    {
+        [TestMethod]
+        public void DefaultDictTest()
+        {
+            var dd = new DefaultDictionary<int, int>();
+            dd[0] = 1;
+            dd[1]++;
+            
+            Assert.AreEqual(1, dd[0]);
+            Assert.AreEqual(1, dd[1]);
+            Assert.AreEqual(0, dd[2]);
+            Assert.AreEqual(3, dd.Count);
+        }
+    }
+}
