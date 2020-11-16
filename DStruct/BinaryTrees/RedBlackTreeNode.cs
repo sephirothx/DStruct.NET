6cb9bfa8f6aecebc5254a47fc031fe2900d23653
Stefano Prosperi <step.prosperi@gmail.com>
2020-11-16 17:22:57 +0100

    Updated RedBlackTreeNode

--- DStruct/BinaryTrees/RedBlackTreeNode.cs
+++ DStruct/BinaryTrees/RedBlackTreeNode.cs
@@ -15,7 +15,7 @@
         // Since the number of left children is always non negative, this cannot cause loss of information.
         private int _info;
 
-        public bool IsRed => (_info & 1 << 31) != 0;
+        public bool IsRed => (_info >> 31) != 0;
 
         public int LeftChildren
         {
@@ -26,9 +26,12 @@
         public bool IsRoot => Parent == null;
 
         public bool IsLeftChild => Parent?.Left == this;
-
+        public bool IsRightChild => Parent?.Right == this;
         public bool HasTwoRedChildren => (Left?.IsRed ?? false) && (Right?.IsRed ?? false);
-
+        public bool HasARedChild => (Left?.IsRed ?? false) || (Right?.IsRed ?? false);
+        public RedBlackTreeNode<T> OuterChild => IsLeftChild ? Left : Right;
+        public RedBlackTreeNode<T> InnerChild => IsLeftChild ? Right : Left;
+        public RedBlackTreeNode<T> Sibling => IsLeftChild ? Parent?.Right : Parent?.Left;
         public RedBlackTreeNode<T> GrandParent => Parent?.Parent;
 
         public RedBlackTreeNode(T value, RedBlackTreeNode<T> parent = null, bool isRed = true, int leftChildren = 0)
@@ -52,9 +55,40 @@
             tmp.Parent = Parent;
             Parent     = tmp;
 
+            if (Right != null)
+            {
+                Right.Parent = this;
+            }
+
             tmp.LeftChildren += LeftChildren + 1;
 
             return tmp;
+        }
+
+        public void RotateLeft2()
+        {
+            var tmp = Right;
+
+            if (IsLeftChild)
+            {
+                Parent.Left = tmp;
+            }
+            else if (Parent != null)
+            {
+                Parent.Right = tmp;
+            }
+
+            Right      = tmp.Left;
+            tmp.Left   = this;
+            tmp.Parent = Parent;
+            Parent     = tmp;
+
+            if (Right != null)
+            {
+                Right.Parent = this;
+            }
+
+            tmp.LeftChildren += LeftChildren + 1;
         }
 
         public RedBlackTreeNode<T> RotateRight()
@@ -66,9 +100,40 @@
             tmp.Parent = Parent;
             Parent     = tmp;
 
+            if (Left != null)
+            {
+                Left.Parent = this;
+            }
+
             LeftChildren -= tmp.LeftChildren + 1;
 
             return tmp;
+        }
+
+        public void RotateRight2()
+        {
+            var tmp = Left;
+
+            if (IsLeftChild)
+            {
+                Parent.Left = tmp;
+            }
+            else if (Parent != null)
+            {
+                Parent.Right = tmp;
+            }
+
+            Left       = tmp.Right;
+            tmp.Right  = this;
+            tmp.Parent = Parent;
+            Parent     = tmp;
+
+            if (Left != null)
+            {
+                Left.Parent = this;
+            }
+
+            LeftChildren -= tmp.LeftChildren + 1;
         }
 
         public RedBlackTreeNode<T> PerformRotation()
@@ -101,14 +166,184 @@
             return ret;
         }
 
+        public static T RemoveInOrderSuccessor(RedBlackTreeNode<T> node, ref RedBlackTreeNode<T> newRoot)
+        {
+            if (node.Left != null)
+            {
+                node.LeftChildren--;
+                T ret1 = RemoveInOrderSuccessor(node.Left, ref newRoot);
+
+                return ret1;
+            }
+
+            T ret = node.Value;
+            node.Delete(ref newRoot);
+            return ret;
+        }
+
+        public static T RemoveInOrderPredecessor(RedBlackTreeNode<T> node, ref RedBlackTreeNode<T> newRoot)
+        {
+            if (node.Right != null)
+            {
+                T ret1 = RemoveInOrderPredecessor(node.Right, ref newRoot);
+
+                return ret1;
+            }
+
+            T ret = node.Value;
+            node.Delete(ref newRoot);
+            return ret;
+        }
+
         public void Recolor()
         {
             _info ^= 1 << 31;
         }
 
+        public void RecolorNodeAndChildren()
+        {
+            Recolor();
+            Left.Recolor();
+            Right.Recolor();
+        }
+
         public void ColorBlack()
         {
             _info &= ~(1 << 31);
+        }
+
+        public void Delete(ref RedBlackTreeNode<T> newRoot)
+        {
+            RedBlackTreeNode<T> ret = null;
+
+            if (IsRoot        &&
+                Left  == null &&
+                Right == null)
+            {
+                newRoot = null;
+                return;
+            }
+
+            if (!(IsRed || HasARedChild))
+            {
+                ret = FixDoubleBlack();
+            }
+
+            var child = Left ?? Right;
+
+            if (IsRoot)
+            {
+                ret = child;
+            }
+
+            child?.Substitute();
+
+            if (IsLeftChild)
+            {
+                Parent.Left = child;
+            }
+            else if (IsRightChild)
+            {
+                Parent.Right = child;
+            }
+
+            if (ret != null)
+            {
+                newRoot = ret;
+            }
+        }
+
+        public RedBlackTreeNode<T> FixDoubleBlack()
+        {
+            if (IsRoot)
+            {
+                return this;
+            }
+
+            RedBlackTreeNode<T> ret = null;
+
+            var sibling = Sibling;
+            var parent  = Parent;
+
+            if (sibling.IsRed)
+            {
+                if (parent.IsRoot)
+                {
+                    ret = sibling;
+                }
+
+                sibling.Recolor();
+                parent.Recolor();
+                if (sibling.IsLeftChild)
+                {
+                    parent.RotateRight2();
+                }
+                else
+                {
+                    parent.RotateLeft2();
+                }
+
+                FixDoubleBlack();
+            }
+            else if (sibling.OuterChild?.IsRed ?? false)
+            {
+                if (parent.IsRoot)
+                {
+                    ret = sibling;
+                }
+
+                sibling.OuterChild.ColorBlack();
+                if (parent.IsRed)
+                {
+                    parent.ColorBlack();
+                    sibling.Recolor();
+                }
+
+                if (sibling.IsLeftChild)
+                {
+                    parent.RotateRight2();
+                }
+                else
+                {
+                    parent.RotateLeft2();
+                }
+            }
+            else if (sibling.InnerChild?.IsRed ?? false)
+            {
+                sibling.Recolor();
+                sibling.InnerChild.Recolor();
+                if (sibling.IsLeftChild)
+                {
+                    sibling.RotateLeft2();
+                }
+                else
+                {
+                    sibling.RotateRight2();
+                }
+
+                ret = FixDoubleBlack();
+            }
+            else
+            {
+                sibling.Recolor();
+                if (parent.IsRed)
+                {
+                    parent.Recolor();
+                }
+                else
+                {
+                    ret = parent.FixDoubleBlack();
+                }
+            }
+
+            return ret;
+        }
+
+        public void Substitute()
+        {
+            Parent = GrandParent;
+
+            ColorBlack();
         }
     }
 }
