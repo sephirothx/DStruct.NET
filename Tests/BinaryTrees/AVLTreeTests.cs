24461153d16b98d3c0c6168d4ee4d2216cda11f1
Paolo Bonanno <paolobonanno@incaricotech.com>
2020-12-09 19:52:55 +0100

    avl tree enumerable test

--- Tests/BinaryTrees/AVLTreeTests.cs
+++ Tests/BinaryTrees/AVLTreeTests.cs
@@ -1,5 +1,6 @@
 using DStruct.BinaryTrees;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Tests.BinaryTrees
 {
@@ -44,6 +45,78 @@ namespace Tests.BinaryTrees
             Assert.IsFalse(avl.Remove(-10));
 
             Assert.AreEqual(9, avl.Count);
+        }
+
+        [TestMethod]
+        public void EnumerateInOrderTraverse()
+        {
+            var list = new[] { 6, 4, 2, 1, 3, 7, 9, 8, 10 };
+            var avl = new AVLTree<int>();
+
+            foreach (int i in list)
+            {
+                avl.Insert(i);
+            }
+
+            var orderedSequence = Enumerable.Range(1, 4).Concat(Enumerable.Range(6, 5)).ToList();
+
+            var actualOrdered = avl.InOrderTraverse().ToList();
+
+            CollectionAssert.AreEqual(orderedSequence, actualOrdered);
+        }
+
+        [TestMethod]
+        public void EnumeratePostOrderTraverse()
+        {
+            var list = new[] { 6, 4, 2, 1, 3, 7, 9, 8, 10 };
+            var avl = new AVLTree<int>();
+
+            foreach (int i in list)
+            {
+                avl.Insert(i);
+            }
+
+            var orderedSequence = Enumerable.Range(1, 4).Concat(Enumerable.Range(6, 5)).Reverse().ToList();
+
+            var actualOrdered = avl.PostOrderTraverse().ToList();
+
+            CollectionAssert.AreEqual(orderedSequence, actualOrdered);
+        }
+
+        [TestMethod]
+        public void EnumeratePreOrderTraverse()
+        {
+            var list = new[] { 2, 3, 5, 6, 9, 10 };
+            var avl = new AVLTree<int>();
+
+            foreach (int i in list)
+            {
+                avl.Insert(i);
+            }
+
+            var orderedSequence = new[] { 6, 3, 2, 9, 5, 10 }.ToList();
+
+            var actualOrdered = avl.PreOrderTraverse().ToList();
+
+            CollectionAssert.AreEqual(orderedSequence, actualOrdered);
+        }
+
+        [TestMethod]
+        public void EnumerateBreadthFirstSearch()
+        {
+            var list = new[] { 2, 3, 5, 6, 9, 10 };
+            var avl = new AVLTree<int>();
+
+            foreach (int i in list)
+            {
+                avl.Insert(i);
+            }
+
+            var orderedSequence = new[] { 6, 3, 9, 2, 5, 10 }.ToList();
+
+            var actualOrdered = avl.BreadthFirstSearch().ToList();
+
+            CollectionAssert.AreEqual(orderedSequence, actualOrdered);
         }
     }
 }
